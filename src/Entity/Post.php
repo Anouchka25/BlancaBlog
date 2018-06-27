370194aec031bb2c3e31127d540f342088187dea
Anouchka MINKOUE OBAME <informaticaux@gmail.com>
2018-06-27 14:08:50 +0200

    aaaaaaaaaaaa

--- src/Entity/Post.php
+++ src/Entity/Post.php
@@ -14,6 +14,7 @@ namespace App\Entity;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
 use Symfony\Component\Validator\Constraints as Assert;
 
 /**
@@ -146,6 +147,7 @@ class Post
         $this->publishedAt = new \DateTime();
         $this->comments = new ArrayCollection();
         $this->tags = new ArrayCollection();
+        $this->image = new UploadedFile();
     }
 
     public function getId(): int
